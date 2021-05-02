e37dd1a610deb8d374d7032b4320bcbdda44bd8e
Matthew William Reidy <48036844+squidy97@users.noreply.github.com>
2021-05-02 15:49:56 -0700

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -1,27 +1,22 @@
-package twosum;
-
-public class twosum {
- 
-
-	    public static void twoSum(int[] nums, int target) {
+	    public static void twoSum(int[] nums, int target) 
+	      {
 	    	int result=0; 
 	        for(int i = 0; i<nums.length ;i++)
 	        	{
 	            for(int j=0; j<nums.length ;j++ )
 	              {
 	            	result = nums[i] + nums[j];
-	            	if(result==target) {
-	            	System.out.println(result);
-	            	System.out.println(nums[i]);
-	            	System.out.println(nums[j]);
-
+	            	if(result==target) 
+	            	{
 	            	
-	              }	            
-	           }
-	        }
+	            	System.out.print(nums[i]);          	
+	                }	            
+	              }
+	        	}
 
-	    }
-		public static void main(String[] args) {
+	    	}
+		public static void main(String[] args) 
+		{
 			int[] nums= {2,15,11,7};
 			int target= 9;
 			
@@ -29,5 +24,3 @@ public class twosum {
 		}
 	        }
 	
-
-
